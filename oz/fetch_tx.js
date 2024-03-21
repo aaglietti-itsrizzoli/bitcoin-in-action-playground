3add8d6354bab1af5bcca7641b46475c6650e2e7
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-03-21 21:22:00 +0000

    fix: add new examples on oz

--- oz/fetch_tx.js
+++ oz/fetch_tx.js
@@ -4,6 +4,7 @@
 const bitcoin = require('bitcoinjs-lib')
 const { RegtestUtils } = require('regtest-client')
 const regtestUtils = new RegtestUtils({APIURL: 'http://bitcoinjs-regtest-server:8080/1'})
+const sleep = ms => new Promise(r => setTimeout(r, ms))
 
 const network = regtestUtils.network // regtest network params
 
@@ -11,9 +12,13 @@ const keyPair = bitcoin.ECPair.makeRandom({ network })
 const p2pkh = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network })
 
 ;(async () => {
+    // a new address
+    const address = p2pkh.address
+    console.log('a new address', address)
+
     // Tell the server to send you coins (satoshis)
     // Can pass address
-    const unspent = await regtestUtils.faucet(p2pkh.address, 2e4)
+    const unspent = await regtestUtils.faucet(address, 2e4)
     console.log('unspent', {unspent})
 
     // Get all current unspents of the address.
@@ -23,4 +28,38 @@ const p2pkh = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network })
     // Get data of a certain transaction
     const fetchedTx = await regtestUtils.fetch(unspent.txId)
     console.log('fetchedTx', {fetchedTx})
+
+    const mineResult = await regtestUtils.mine(2)
+    console.log("mine result", mineResult)
+
+    const unspentComplex = await regtestUtils.faucetComplex(p2pkh.output, 1e4)
+
+    await sleep(100);
+
+    const txb = new bitcoin.TransactionBuilder(network)
+    txb.addInput(unspent.txId, unspent.vout)
+    txb.addInput(unspentComplex.txId, unspentComplex.vout)
+    txb.addOutput(regtestUtils.RANDOM_ADDRESS, 1e4)
+
+    txb.sign({
+      prevOutScriptType: 'p2pkh',
+      vin: 0,
+      keyPair,
+    })
+    txb.sign({
+      prevOutScriptType: 'p2pkh',
+      vin: 1,
+      keyPair,
+    })
+    const tx = txb.build()
+
+    // build and broadcast to the Bitcoin RegTest network
+    await regtestUtils.broadcast(tx.toHex())
+
+    await regtestUtils.verify({
+      txId: tx.getId(),
+      address: regtestUtils.RANDOM_ADDRESS,
+      vout: 0,
+      value: 1e4
+    })
 })();
